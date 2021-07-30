5b0d0deefc9a7846b960017dc881878fab6b7291
JoschkaSchulz <Horstyschulz@gmail.com>
2021-07-30 17:04:59 +0200

    fix more stupid typos
    at least it was wrong everywhere

---
+++ plantdoctor_frontend/src/views/LeafDamage.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-container>
+    <leaf-damage-form />
+  </v-container>
+</template>
+
+<script>
+import LeafDamageForm from "@/components/LeafDamageForm";
+export default {
+  name: "LeafDamage",
+  components: {LeafDamageForm}
+}
+</script>
